b826821b0e1d388a3f4cf624d80fccdca530fd39
Nathan Romero <schmutts@yahoo.ca>
2025-04-28 00:43:45 -0400

    Completed Exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,19 +6,23 @@ const Header = ({ course }) => {
   )
 }
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+const Part = ({ part, exercises }) => {
   return (
     <>
       <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
+        {part} {exercises}
       </p>
     </>
+  )
+}
+
+const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
   )
 }
 
