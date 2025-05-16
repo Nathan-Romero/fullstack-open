1fe06ff8219de5e36d37d385d43f86eed9c43064
Nathan Romero <schmutts@yahoo.ca>
2025-05-16 18:53:39 -0400

    Completed Exercise 4.8

---
+++ blog-list/backend/tests/test_helper.js
@@ -0,0 +1,19 @@
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  }
+  // Add more test blogs as needed
+]
+
+module.exports = {
+  initialBlogs
+}
