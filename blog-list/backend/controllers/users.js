0e102c973ed5d936f8c698a4328843848a3e8df6
Nathan Romero <schmutts@yahoo.ca>
2025-05-17 21:29:25 -0400

    Completed Exercise 4.15

---
+++ blog-list/backend/controllers/users.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+usersRouter.delete('/:id', async (request, response) => {
+  try {
+    const result = await User.findByIdAndDelete(request.params.id)
+
+    if (result) {
+      response.status(204).end()
+    } else {
+      response.status(404).json({ error: 'user not found' })
+    }
+  } catch {
+    response.status(400).json({ error: 'malformatted id' })
+  }
+})
+
+module.exports = usersRouter
--- blog-list/backend/tests/test_helper.js
+++ blog-list/backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -15,11 +16,6 @@ const initialBlogs = [
   }
 ]
 
-const blogsInDb = async () => {
-  const blogs = await Blog.find({})
-  return blogs.map(blog => blog.toJSON())
-}
-
 const nonExistingId = async () => {
   const blog = new Blog({
     title: 'Test Title',
@@ -33,8 +29,19 @@ const nonExistingId = async () => {
   return blog._id.toString()
 }
 
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
   initialBlogs,
+  nonExistingId,
   blogsInDb,
-  nonExistingId
+  usersInDb,
 }
